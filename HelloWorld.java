bc584fdbb1e98d471b0dcbb64cc99e620e3c96b5
siddeshwer <siddeshwer90@gmail.com>
2021-04-27 08:50:09 +0530

    merge

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main(String[] args) {
         int a=2,b=2,c;
 		System.out.println(c=10000*b); 
+	        System.out.println("Welcome to SAPIENT");
     }
 }
